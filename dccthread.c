696fb822e6fdac986711d02858f3569594772b8a
Henrique Rotsen <henriquerotsensf@gmail.com>
2023-05-02 22:56:51 -0300

    Início da implementação

--- dccthread.c
+++ dccthread.c
@@ -7,10 +7,56 @@
 #include <malloc.h>
 #include <ucontext.h>
 
-void dccthread_init(void (*func)(int), int param){
+typedef struct dccthread
+{
+    ucontext_t context;
+    char name[DCCTHREAD_MAX_NAME_SIZE];
+
+} dccthread_t;
+
+dccthread_t manager;
+dccthread_t * main;
+
+void schedule(int param){
+    while (!dlist_empty(ready)){
+        /* code */
+    }
     
 }
 
-dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param){
+void dccthread_init(void (*func)(int), int param)
+{
+    // iniciando manager
+    getcontext(&manager.context);
+    manager.context.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
+    manager.context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    strcpy(manager.name, "manager");
+    makecontext(&manager.context, (void *)schedule, 0, NULL);
 
+    main = dccthread_create("main", func, param);
+
+    struct dlist * ready = dlist_create();
+    dlist_push_right(ready, main);
+
+    setcontext(&main->context);
+}
+
+dccthread_t *dccthread_create(const char *name, void (*func)(int), int param)
+{
+    dccthread_t *newThread = malloc(sizeof(dccthread_t));
+
+// 
+    getcontext(&newThread->context);                               // inicializando ucontext
+    newThread->context.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE); // informando *ptr
+    newThread->context.uc_stack.ss_size = THREAD_STACK_SIZE;       // informando stack size max
+    newThread->context.uc_link = &manager.context;                 // informando thread de troca de contexto
+    strcpy(newThread->name, name);
+// 
+
+    makecontext(&newThread->context, (void *)func, 0, NULL); // assinalando uma função para a thread nova
+    return newThread;
+}
+
+void dccthread_yield(void)
+{
 }
